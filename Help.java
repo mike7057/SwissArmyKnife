7e0f0d4c4b36dd0151817c909c1b4375ef9f9542
mike7057 <46804847+mike7057@users.noreply.github.com>
2021-03-28 15:39:49 -0500

    Add files via upload

---
+++ Help.java
@@ -0,0 +1,12 @@
+public class Help {
+    public static void printHelp() {
+    System.out.println("Here is a helpful list of arguments to get you started in using SwissArmyKnife");
+    System.out.println("");
+    System.out.println("java sak -HttpRequest followed by a URL will return websites data");
+    System.out.println("java sak -HttpRequest [URL]");
+    System.out.println("java sak -HttpRequest https://www.google.com");
+    System.out.println("java sak -HttpRequest https://www.dominos.com");
+    System.out.println("");
+    System.out.println("");
+    }
+}
---
+++ sak.java
@@ -0,0 +1,47 @@
+
+
+public class sak {
+    
+    public static void main(String[] args) {
+
+        System.out.println("");
+        System.out.println("");
+
+        //Outputs text and -help if argument length is less than 1, basically if no argument was input
+        //Checks argument length is less than 1
+        if (args.length < 1) {  
+            System.out.println("It appears you did not input an argument. You can use 'java sak -Help' for a list of commands");
+            System.out.println("");
+            //Calls printHelp in Help.java
+            Help.printHelp(); 
+
+        //Outputs -Help text showing user commands if -Help argument is used
+        //Checks if args is equal to -Help and ignores any case used
+        } else if  (args[0].equalsIgnoreCase("-Help")) {
+            System.out.println("Help to the rescue!!!");
+            //Calls printHelp in Help.java
+            Help.printHelp(); 
+        
+        
+        } else if (args[0].equalsIgnoreCase("-HttpRequest")) { //Checks if args is equal to -HttpRequest
+            System.out.println("Executing HttpRequest...");
+
+            if (args.length < 2) { //Checks for args length of less than 2
+                System.out.println("In order to use -HttpRequest you need to provide a valid URL as the second parameter");
+
+            //If args length is more than 2, the URL is passed to HttpRequest.java to get the websites data
+            } else { 
+                String URL = args[1];
+                HttpRequest request = new HttpRequest();
+
+                if (request.readURL(URL)) {
+                    System.out.println(request);
+                    System.out.println("index printed");
+                }
+            }
+        }
+
+         System.out.println("");
+        
+    } 
+} 
